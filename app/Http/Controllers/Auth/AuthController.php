84abc0ba4f83378b9717fc1c860b33b8301965d0
0xc5f4 <ilovemonster@126.com>
2017-11-23 22:34:28 +0800

    update auth

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,8 +6,7 @@ use App\User;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
-use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 use App\Extensions\AuthenticatesLogout;
 
 class AuthController extends Controller
@@ -22,10 +21,10 @@ class AuthController extends Controller
     | a simple trait to add these behaviors. Why don't you explore it?
     |
     */
-    use AuthenticatesUsers, AuthenticatesLogout {
-        AuthenticatesLogout::logout insteadof AuthenticatesUsers;
+    use AuthenticatesAndRegistersUsers, AuthenticatesLogout {
+        AuthenticatesLogout::logout insteadof AuthenticatesAndRegistersUsers;
     }
-    use RegistersUsers, ThrottlesLogins;
+    use ThrottlesLogins;
 
     /**
      * Where to redirect users after login / registration.
